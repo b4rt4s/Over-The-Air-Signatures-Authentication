8e1f8031098f970f9279ae39cc4bc383098bafe6
Bartłomiej <bartlomiej.baut35@gmail.com>
2024-10-15 10:30:03 +0200

    Code cleanup of signatureCollecting module

--- signatures-collecting/signatureCollecting.py
+++ signatures-collecting/signatureCollecting.py
@@ -1,6 +1,9 @@
 from airSigning import AirSigning
+from handDetector import handDetector
 
 if __name__ == "__main__":
 
-    signComponent = AirSigning()
+    handComponent = handDetector()
+    signComponent = AirSigning(handComponent)
+
     signComponent.drawSign()
