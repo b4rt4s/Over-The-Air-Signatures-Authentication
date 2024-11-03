5a6066fcafb69d77cdea7e4c94edbe31d85f15dc
Bartłomiej <bartlomiej.baut35@gmail.com>
2024-11-03 05:16:47 +0100

    Basic plot

--- classifiers/plot.py
+++ classifiers/plot.py
@@ -2,22 +2,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-# Wczytywanie danych z pliku
-data = pd.read_csv('far_frr_results.txt', header=None, names=['FAR', 'FRR', 'Threshold'])
+# Załaduj dane z pliku, przekształcając przecinki na kropki w przypadku użycia przecinka jako separatora dziesiętnego
+data = pd.read_csv('far_frr_results.txt', converters={
+    'far': lambda x: float(x.replace(',', '.')),
+    'frr': lambda x: float(x.replace(',', '.'))
+})
 
-# Sortowanie danych według kolumny 'Threshold'
-data.sort_values('Threshold', inplace=True)
+# Sortowanie danych według kolumny 'Threshold' (jeśli dane nie są posortowane)
+data.sort_values('threshold', inplace=True)
 
 # Wyznaczanie punktu EER
-differences = np.abs(data['FAR'] - data['FRR'])
+differences = np.abs(data['far'] - data['frr'])
 min_index = np.argmin(differences)
-EER = (data['FAR'][min_index] + data['FRR'][min_index]) / 2
-EER_threshold = data['Threshold'][min_index]
+EER = (data['far'].iloc[min_index] + data['frr'].iloc[min_index]) / 2
+EER_threshold = data['threshold'].iloc[min_index]
 
 # Rysowanie wykresu
 plt.figure(figsize=(10, 5))
-plt.plot(data['Threshold'], data['FAR'], label='FAR (%)', marker='o')
-plt.plot(data['Threshold'], data['FRR'], label='FRR (%)', marker='x')
+plt.plot(data['threshold'], data['far'], label='FAR (%)', marker='o')
+plt.plot(data['threshold'], data['frr'], label='FRR (%)', marker='x')
 plt.xlabel('Threshold')
 plt.ylabel('Error Rate (%)')
 plt.title('FAR and FRR vs. Threshold with EER Point')
